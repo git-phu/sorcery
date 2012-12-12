f7a19d25015f517b7500ae5ffea446fa009d3981
Peter Hu <peter@identified.com>
2012-12-11 16:45:17 -0800

    assume providers_class is Service::Base (so that we don't have to use the Service module as the base, which seems to randomly fail)

--- lib/sorcery/model/submodules/external.rb
+++ lib/sorcery/model/submodules/external.rb
@@ -59,8 +59,8 @@ module Sorcery
             if not config.provider_attribute_is_key
               authentication = config.authentications_class.find_by_provider_and_uid(provider_name, uid)
             else
-              provider = config.providers_class.const_get(provider_name.capitalize) if config.providers_class.const_defined? provider_name.capitalize
-              authentication = config.authentications_class.where(config.provider_attribute_name => provider.service_id, config.provider_uid_attribute_name => uid).first if provider
+              provider = config.providers_class.parent.const_get(provider_name.capitalize)
+              authentication = config.authentications_class.where(config.provider_attribute_name => provider.service_id, config.provider_uid_attribute_name => uid).first
             end
              user = find(authentication.send(config.authentications_user_id_attribute_name)) if authentication
           end
